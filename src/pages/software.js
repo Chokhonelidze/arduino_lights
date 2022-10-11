204a7744d4b44e808764a029e4b512fa3b4f7ee5
George Chokhonelidze <g_chokhonelidze@outlook.com>
2022-10-10 19:33:52 -0500

    url change

--- src/pages/software.js
+++ src/pages/software.js
@@ -3,7 +3,7 @@ import "./software.css";
 export default function Software(){
     return <div className="software">
         <div className="code">
-            <a href="/arduino_lights/code/arduino_lights.ino" className="btn btn-primary m-3" role="button">Download</a>
+            <a href="/arduino_lights/codes/arduino_lights.ino" className="btn btn-primary m-3" role="button">Download</a>
         <iframe title='Lights' src="/arduino_lights/codes/arduino_lights.html" className="iframe"></iframe>
         </div>
     </div>
